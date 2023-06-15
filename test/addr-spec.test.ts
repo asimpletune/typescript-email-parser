406fea13d16586f4fd9b4613519e6fd6ce63ee62
Spencer Scorcelletti <2670813+asimpletune@users.noreply.github.com>
2023-06-15 23:38:36 +0200

    Message spec (#5)
    * Add a few tests for the addr-spec
    
    * Refactor part of spec to use hex values directly, and start updating REAMDE
    
    * Finish first real text for dot-atom-text
    
    * Update .gitattributes file to not show diffs for generated code
    
    * Add CFWS tests
    
    * Add tests for quoted-string
    
    * Add dot-atom tests for domain as well as a 'parts' computed field on dot-atom types
    
    * Add the bigger 'address' spec (note: not 'addr-spec')
    
    * Add header rules + date time rules
    
    * Add general message body spec
    
    * Update README

--- test/addr-spec.test.ts
+++ test/addr-spec.test.ts
@@ -64,6 +64,52 @@ describe('addr-spec', () => {
         let domain_literal = domain.token as domain_literal
         expect(domain_literal.contents).toBe('dt(e)xt')
       })
+      describe('CFWS', () => {
+        test('a single space', () => {
+          expect(parse(' abc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        })
+        test('multiple tabs', () => {
+          expect(parse('\t\tabc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        })
+        test('CRLF followed by space', () => {
+          expect(parse('\r\n abc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        })
+        test('comment before FWS', () => {
+          expect(parse('(comment)\r\n abc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        })
+      })
+    })
+    describe('quoted-string', () => {
+      test('qcontent', () => {
+        let local_part = parse('"abc"@domain').ast!.local_part
+        expect(local_part.token.kind).toBe(ASTKinds.quoted_string)
+        let quoted_string = local_part.token as quoted_string
+        expect(quoted_string.contents).toBe("abc")
+      })
+      describe('CFWS', () => {
+        test('a single space', () => {
+          expect(parse(' "abc.def"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('multiple tabs', () => {
+          expect(parse('"\t\tabc.def"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('CRLF followed by space', () => {
+          expect(parse('\r\n "abc"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('Mixture of CFWS and FWS', () => {
+          expect(parse('(comment)\r\n "\r\n abc "\r\n (comment)@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+      })
+    })
+  })
+  describe('domain', () => {
+    describe('dot-atom', () => {
+      test('dot-atom-text', () => {
+        let domain = parse('a@def.ghi.jkl').ast!.domain
+        expect(domain.token.kind).toBe(ASTKinds.dot_atom)
+        let dot_atom = domain.token as dot_atom
+        expect(dot_atom.parts).toEqual(['def', 'ghi', 'jkl'])
+      })
     })
   })
 })
