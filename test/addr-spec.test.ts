06a6a8f0a681d916335357b325bd0dc84aa86575
Spencer Scorcelletti <contact@spenc.es>
2023-06-15 16:12:55 +0200

    Finish first real text for dot-atom-text

--- test/addr-spec.test.ts
+++ test/addr-spec.test.ts
@@ -1,12 +1,16 @@
 import { describe, expect, test } from '@jest/globals';
-import { ASTKinds, parse } from '../parsers/addr-spec'
+import { ASTKinds, dot_atom, parse } from '../parsers/addr-spec'
 
 describe('addr-spec', () => {
   describe('local-part', () => {
     describe('dot-atom', () => {
       test("dot-atom-text", () => {
-        let act = parse("abc.def@domain")
-        expect(act.ast?.local_part.text.kind).toBe(ASTKinds.dot_atom)
+        let ast = parse('abc.def@domain').ast!
+        expect(ast.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        let dot_atom = (ast.local_part.token as dot_atom)
+        expect(dot_atom.head).toBe('abc')
+        expect(dot_atom.tail).toEqual(['def'])
+        expect(dot_atom.literal).toBe('abc.def')
       })
     })
   })
