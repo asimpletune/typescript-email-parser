566101e11956f576265664a83a04035594def0e4
Spencer Scorcelletti <contact@spenc.es>
2023-06-15 16:57:57 +0200

    Add tests for quoted-string

--- test/addr-spec.test.ts
+++ test/addr-spec.test.ts
@@ -74,6 +74,31 @@ describe('addr-spec', () => {
         test('CRLF followed by space', () => {
           expect(parse('\r\n abc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
         })
+        test('comment before FWS', () => {
+          expect(parse('(comment)\r\n abc.def@domain').ast?.local_part.token.kind).toBe(ASTKinds.dot_atom)
+        })
+      })
+    })
+    describe('quoted-string', () => {
+      test('qcontent', () => {
+        let local_part = parse('"abc"@domain').ast!.local_part
+        expect(local_part.token.kind).toBe(ASTKinds.quoted_string)
+        let quoted_string = local_part.token as quoted_string
+        expect(quoted_string.contents).toBe("abc")
+      })
+      describe('CFWS', () => {
+        test('a single space', () => {
+          expect(parse(' "abc.def"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('multiple tabs', () => {
+          expect(parse('"\t\tabc.def"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('CRLF followed by space', () => {
+          expect(parse('\r\n "abc"@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
+        test('Mixture of CFWS and FWS', () => {
+          expect(parse('(comment)\r\n "\r\n abc "\r\n (comment)@domain').ast?.local_part.token.kind).toBe(ASTKinds.quoted_string)
+        })
       })
     })
   })
