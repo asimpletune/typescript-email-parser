3fefad248241f34ef6e14109f6d797a2bdff9ad4
Spencer Scorcelletti <contact@spenc.es>
2023-06-17 00:04:24 +0200

    Fix nested kleene star bug in tsPEG and change import to patch version while patch is still in review

--- test/message.test.ts
+++ test/message.test.ts
@@ -1,8 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
-import { Parser as MsgParser, parse as msgParse, dot_atom as dot_atom2 } from '../test/message.parser';
+import { Parser, parse, dot_atom as dot_atom2 } from '../message.parser';
 
-let msg = `
-Subject: Hello
+let msg = `Subject: Hello
 Mime-Version: 1.0 (Mac OS X Mail 16.0 \(3731.500.231\))
 Content-Type: multipart/alternative;
 	boundary="Apple-Mail=_7B18F409-7DD5-4592-9AFB-A648138D421F"
@@ -35,19 +34,6 @@ Content-Type: text/html;
 
 describe('message', () => {
   test('parser', () => {
-    // let ast = msgParse(msg)
-
-    let msgParser = new MsgParser("bob@foo.com")
-    let matchMethods = Object.getOwnPropertyNames(MsgParser.prototype).filter(name => name.startsWith("match") && !["matchstart", "matchmessage", "matchunstructured_2", "matchmessage_$0", "matchmessage_$0_1",
-      "matchbody",
-      "matchbody_2", "matchfields", "matchobs_body", "matchobs_unstruct"].includes(name))
-    for (const method of matchMethods) {
-      console.log(`Trying method ${method}`);
-      msgParser[method](0)
-      console.log(`Method terminated\n\n`);
-    }
-
-    // expect(matchMethods).toBe(123)
-    // expect(foo.literal).toBe("bob")
+    expect(parse(msg).errs).toEqual([])
   })
 })
